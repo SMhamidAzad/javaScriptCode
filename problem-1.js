9d89cd07ff89cdda90347bb524f931a989b28a4e
SMhamidAzad <hamidazad020@gmail.com>
2022-01-29 20:44:35 +0600

    first commit

---
+++ problem-1.js
@@ -0,0 +1,40 @@
+// 13 er namata;
+
+    // function Namata(num){
+    //     for(var i=1;i<=10;i++)
+    //     {
+    //         console.log(`${num} X ${i} = ${num*i}`);
+    //     }
+    // }
+    // Namata(13);
+
+// uppercase to lowercase convert
+
+    // function regularToLower(name){
+    //      var result = name.toLowerCase();
+    //      return result;
+    // }
+
+    // var yourName = "Hamid Azad";
+    // var lowerCaseName = regularToLower(yourName);
+    // console.log(lowerCaseName);
+
+// return fullname
+
+    // function yourFullName(fname,lname){
+    //    var fullName = fname + ' '+ lname;
+    //    return fullName;
+    // }
+    // var firstName = 'hablu';
+    // var lastName = 'kanto';
+    // var pickYourFullName = yourFullName(firstName,lastName);
+    // console.log(pickYourFullName);
+
+// convert a number in square
+    // function regularToSquare(number){
+    //      var numberToSquare = number*number;
+    //      return numberToSquare;
+    // }
+    // var regularNumber = 5;
+    // var result = regularToSquare(regularNumber);
+    // console.log(result);
